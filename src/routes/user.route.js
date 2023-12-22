c0dea79a1498f51195b8a690db800a398584af71
Prakash Kumar <prakash_raut@outlook.com>
2023-12-22 21:33:31 +0530

    added refreshtoken endpoint

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -27,5 +28,7 @@ userRouter.route("/login").post(loginUser);
 
 // secured routes
 userRouter.route("/logout").post(verifyJWT, logoutUser);
+
+userRouter.route("/refreshToken").post(refreshAccessToken);
 
 export { userRouter };
